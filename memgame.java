d691fda004343c648179d95497ef067ade727ef4
varalakeerthana <varalakeerthana190@gmail.com>
2026-01-08 20:02:53 +0530

    Compiled the program

--- memgame.java
+++ memgame.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class memgame{
   public static void main(String args[]){
     Scanner scanner = new Scanner(System.in);
-    ArrayList<Character> cards = new ArrayList<>();
+    ArrayList<String> cards = new ArrayList<>();
 
     cards.add("A"); cards.add("A");
     cards.add("B"); cards.add("B");
@@ -10,20 +10,20 @@ public class memgame{
     cards.add("D"); cards.add("D");
     Collections.shuffle(cards);
 
-    String board = new String(cards.size());
-    board.fill(" ");
-    boolean[] filled = new boolean(cards.size());
+    String board[] = new String[cards.size()];
+    Arrays.fill(board," ");
+    boolean[] flipped = new boolean[cards.size()];
     int noofPairs =0;
 
     System.out.println("Welcome to the memory game");
-    while(noOfPairs<4){
+    while(noofPairs<4){
       displayBoard(board);
-      firstIndex=getIndex(scanner , board,flipped,"Enter the index to flip the first card:");
+      int firstIndex=getIndex(scanner , board,flipped,"Enter the index to flip the first card:");
       board[firstIndex]=cards.get(firstIndex);
       flipped[firstIndex]=true;
       displayBoard(board);
 
-      secondIndex=getIndex(scanner , board,flipped,"Enter the index to flip the second card:");
+      int secondIndex=getIndex(scanner , board,flipped,"Enter the index to flip the second card:");
       board[secondIndex]=cards.get(secondIndex);
       flipped[secondIndex]=true;
       displayBoard(board);
@@ -42,11 +42,12 @@ public class memgame{
     }
     System.out.println("Congratulations!..You won..");
   }
-  public static int getIndex(Scanner scanner , String board , boolean[] flipped , String prompt){
+  public static int getIndex(Scanner scanner , String board[] , boolean[] flipped , String prompt){
+    int index;
     while(true){
       System.out.print(prompt);
-      int index = scanner.nextInt();
-      if(index<0 || index>=board.length()){
+      index = scanner.nextInt();
+      if(index<0 || index>=board.length){
         System.out.println("Entered an invalid position.");
         //System.out.print("Enter valid position:");
         
@@ -55,17 +56,19 @@ public class memgame{
       }else{
         break;
       }
-      return index;
-    }
-  }
-  public static void displayBoard(String board){
-    int i=0;
-    System.out.print("| ");
-    while(i<board.length()){
-      System.out.print(board[i]);
-      System.out.print(" |");
       
     }
+    return index;
+  }
+  public static void displayBoard(String board[]){
+    int i=0;
+    System.out.print("| ");
+    while(i<board.length){
+      System.out.print(board[i]);
+      System.out.print(" |");
+      i++;
+    }
+    System.out.println();
   }
     
 }
